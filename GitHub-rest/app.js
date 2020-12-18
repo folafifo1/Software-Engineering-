8ed1d33577fade3fed66c3f8c7143ceb6d6406de
Fola Fifo <74255108+folafifo1@users.noreply.github.com>
2020-12-18 19:18:44 +0000

    first access of api looking at repos with most

--- GitHub-rest/app.js
+++ GitHub-rest/app.js
@@ -3,3 +3,13 @@ function handleInput() {
     console.log(username)
 
 }
+const btnRepos = document.getElementById("btnRepos")
+btnRepos.addEventListener("click", getRepos)
+async function getRepos() {
+    
+    const url = "https://api.github.com/search/repositories?q=stars:>100000"
+    const response = await fetch(url)
+    const result = await response.json()
+
+    result.items.forEach(i=>console.log(i.full_name))
+}
