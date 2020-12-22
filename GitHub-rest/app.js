d122a31967f7811b7cf847b4c4a5c84626945dce
Fola Fifo <74255108+folafifo1@users.noreply.github.com>
2020-12-22 14:23:19 +0000

    can now print an individual user positives to the console

--- GitHub-rest/app.js
+++ GitHub-rest/app.js
@@ -1,9 +1,10 @@
 function handleInput() {
     var username = document.getElementById('username').value
-    
+    var token = document.getElementById('token').value
     console.log(username)
     var user = document.getElementById("user");
     user.innerHTML = username;
+    details(username,token)
 
 }
 const btnRepos = document.getElementById("btnRepos")
@@ -16,4 +17,23 @@ async function getRepos() {
 
     result.items.forEach(i=>console.log(i.full_name))
     result.items.forEach(i=>console.log(i.language))
+}
+
+async function details(username,token){
+    let url = `https://api.github.com/users/${username}/repos`
+    let t = await get(url, token)
+    console.log(t)
+}
+async function get(url, token) {
+    const headers = {
+        'Authorization': `Token ${token}`
+    }
+
+    const response = (token == undefined) ? await fetch(url) : await fetch(url, {
+        'method': 'GET',
+        'headers': headers
+    })
+
+    let data = await response.json()
+    return data;
 }
