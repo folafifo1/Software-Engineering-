e70a41dcd833cddbe9b909d5a8d394445615da71
Fola Fifo <74255108+folafifo1@users.noreply.github.com>
2020-12-24 18:38:03 +0000

    can now display user repos on page

--- GitHub-rest/app.js
+++ GitHub-rest/app.js
@@ -11,20 +11,20 @@ const btnRepos = document.getElementById("btnRepos")
 btnRepos.addEventListener("click", getRepos)
 async function getRepos() {
     
-    const url = "https://api.github.com/search/repositories?q=stars:>100000"
-    const response = await fetch(url)
+    let url = "https://api.github.com/search/repositories?q=stars:>100000"
+    let response = await fetch(url)
     const result = await response.json()
     result.items.forEach(i=>{
 
         const anchor = document.createElement("a")
         anchor.href = i.html_url;
-        anchor.textContent = i.name;
+        anchor.textContent = i.full_name;
         divResult.appendChild(anchor)
         divResult.appendChild(document.createElement("br"))
 
 
     })
-    //console.log(result)
+    console.log(result)
     //result.items.forEach(i=>console.log(i.full_name))
     //result.items.forEach(i=>console.log(i.language))
 }
@@ -40,6 +40,20 @@ async function details(username,token){
     num.innerHTML = `<b>ID: </b>${user.id}`;
     let name = document.getElementById('name');
     name.innerHTML = `<b>Name: </b>${user.name}`;
+
+    let url1 = `https://api.github.com/users/${username}/repos`
+     let t = await get(url1, token)
+     t.forEach(i=>{
+
+        const anchor = document.createElement("a")
+        anchor.href = i.html_url;
+        anchor.textContent = i.full_name;
+        divResult1.appendChild(anchor)
+        divResult1.appendChild(document.createElement("br"))
+
+
+    })
+     console.log(t)
     
 }
 async function get(url, token) {
