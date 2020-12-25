6b9763f3336e08d7a7e535facb55c34d015f2a6b
Fola Fifo <74255108+folafifo1@users.noreply.github.com>
2020-12-25 17:10:54 +0000

    few upgrades
    added more user info and now displays repository languages

--- GitHub-rest/app.js
+++ GitHub-rest/app.js
@@ -2,31 +2,34 @@ function handleInput() {
     var username = document.getElementById('username').value
     var token = document.getElementById('token').value
     console.log(username)
-    var user = document.getElementById("user");
-    user.innerHTML = username;
     details(username,token)
     divResult1.innerHTML = " "
+    urls.innerHTML = " "
 }
 const btnRepos = document.getElementById("btnRepos")
 btnRepos.addEventListener("click", getRepos)
 async function getRepos() {
     
-    let url = "https://api.github.com/search/repositories?q=stars:>100000"
+    let url = "https://api.github.com/search/repositories?q=stars:>150000"
     let response = await fetch(url)
     const result = await response.json()
     result.items.forEach(i=>{
 
         const anchor = document.createElement("a")
+        
         anchor.href = i.html_url;
         anchor.textContent = i.full_name;
         divResult.appendChild(anchor)
+        const ty = document.createElement("lab")
+        ty.innerHTML = "<b> - Language: </b>" + i.language; 
+        divResult.appendChild(ty)
         divResult.appendChild(document.createElement("br"))
 
 
     })
     console.log(result)
-    //result.items.forEach(i=>console.log(i.full_name))
-    //result.items.forEach(i=>console.log(i.language))
+    result.items.forEach(i=>console.log(i.full_name))
+    result.items.forEach(i=>console.log(i.language))
 }
 
 async function details(username,token){
@@ -40,21 +43,42 @@ async function details(username,token){
     num.innerHTML = `<b>ID: </b>${user.id}`;
     let name = document.getElementById('name');
     name.innerHTML = `<b>Name: </b>${user.name}`;
+    let loc = document.getElementById('location');
+    loc.innerHTML = `<b>Location: </b>${user.location}`;
+    let foll = document.getElementById('followers');
+    foll.innerHTML = `<b>Followers: </b>${user.followers}`;
 
-    let url1 = `https://api.github.com/users/${username}/repos`
+    const anchor = document.createElement("a")
+        const tp = document.createElement("label")
+        tp.innerHTML = "<b>Link: </b>";
+        
+        urls.appendChild(tp)
+        anchor.href = `${user.html_url}`;
+        anchor.textContent = `${user.html_url}`;
+        urls.appendChild(anchor)
+        urls.appendChild(document.createElement("br"))
+
+    
+    
+    
+    
+        let url1 = `https://api.github.com/users/${username}/repos`
      let t = await get(url1, token)
     
      t.forEach(i=>{
 
         const anchor = document.createElement("a")
         anchor.href = i.html_url;
-        anchor.textContent = i.full_name;
+        anchor.textContent = i.name;
         divResult1.appendChild(anchor)
+        const ty = document.createElement("lab")
+        ty.innerHTML = "<b> - Language: </b>" + i.language;  
+        divResult1.appendChild(ty)
         divResult1.appendChild(document.createElement("br"))
 
 
     })
-
+     console.log(t[0].language)
      console.log(t)
     
 }
